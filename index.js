16defeb660064b5151e37b9f482d0fe7d15db71f
Bobby <btn914@gmail.com>
2023-02-03 18:51:04 -0600

    swiper

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 /* FOR HAMBURGER AND NAVIGATION MENU */
-const hamburger = document.querySelector(".hamburger")
-const nav__menu = document.querySelector(".nav__menu")
-const nav__link = document.querySelectorAll(".nav__link")
+const hamburger = document.querySelector(".hamburger");
+const nav__menu = document.querySelector(".nav__menu");
+const nav__link = document.querySelectorAll(".nav__link");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active")
-  nav__menu.classList.toggle("active")
-  console.log(hamburger)
-  console.log(nav__menu)
-})
+  hamburger.classList.toggle("active");
+  nav__menu.classList.toggle("active");
+  console.log(hamburger);
+  console.log(nav__menu);
+});
 
 nav__link.forEach((nav__link) => {
   nav__link.addEventListener("click", function handleClick(event) {
-    console.log("box clicked", event)
-    hamburger.classList.toggle("active")
-    nav__menu.classList.toggle("active")
-  })
-})
+    console.log("box clicked", event);
+    hamburger.classList.toggle("active");
+    nav__menu.classList.toggle("active");
+  });
+});
 
 //swiper
-//todo Delete installed nodes and use cdn for swiperjs DO NOT use NPM to install library
 
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
@@ -40,22 +39,22 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
   },
-})
+});
 
 /*FOR COLLAPSING CARD DESCRIPTION AND FADE EFFECT*/
 
-const mores = document.querySelectorAll(".readMore")
+const mores = document.querySelectorAll(".readMore");
 
 mores.forEach((more) => {
   more.addEventListener("click", function handleClick(event) {
-    console.log("button clicked", event)
+    console.log("button clicked", event);
     if (more.textContent == "Read more") {
-      more.textContent = "Read less"
+      more.textContent = "Read less";
     } else {
-      more.textContent = "Read more"
+      more.textContent = "Read more";
     }
-    const cardDescription = more.previousElementSibling
+    const cardDescription = more.previousElementSibling;
 
-    cardDescription.classList.toggle("expand")
-  })
-})
+    cardDescription.classList.toggle("expand");
+  });
+});
